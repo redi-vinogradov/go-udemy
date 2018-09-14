081e8655a911ef46ede6a95fa6bdcf07ba959aa5
Anton Vinogradov <anton.vinogradov@thomsonreuters.com>
2018-09-14 09:47:54 -0400

    uploading

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,16 @@
 package main
 
+//import "fmt"
 
 func main() {
 	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+	//cards := newDeck()
+	//cards.saveToFile("my_cards")
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	//hand, remainingDeck := deal(cards, 5)
+	//hand.print()
+	//remainingDeck.print()
 }
 
